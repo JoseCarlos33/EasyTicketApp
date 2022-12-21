64dad1e0a42bc110ddca2cd90af388a6d0146dcc
José Carlos Noronha <joseph.finder33@gmail.com>
2022-12-21 13:06:45 -0300

    :zap: Improve Navegability in Login/Register Pages

--- src/components/molecules/AuthInputs/index.tsx
+++ src/components/molecules/AuthInputs/index.tsx
@@ -8,7 +8,8 @@ import {
 } from './styles';
 import { useFormContext } from 'react-hook-form';
 import { useAuth } from 'src/hooks/useAuth';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 interface AuthFormProps {
   isSignInSelected: boolean;
@@ -18,9 +19,19 @@ function AuthInputs({
   isSignInSelected
 }: AuthFormProps) {
   const { signIn, signed, signOut} = useAuth();
-  const { handleSubmit, formState: { errors } } = useFormContext();
+  const { handleSubmit, formState: { errors }, reset } = useFormContext();
+  const navigation = useNavigation<any>();
 
   async function handleSubmitRegister(values: any) {
+    if(!isSignInSelected){
+      Alert.alert("Conta criada com sucesso!")
+      navigation.navigate("LoginRegister", {
+        isSignInSelected: true
+      })
+      reset();
+      return;
+    }
+
     let data = {
       email: values.email as string,
       password: values.password as string
@@ -61,6 +72,7 @@ function AuthInputs({
           <ControlledInput
             name="confirmPassword"
             label='Confirmar senha'
+            secureTextEntry
           />
         )
       }
